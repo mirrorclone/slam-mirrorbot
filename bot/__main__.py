fb9304c8cf83c58f20d449a9a40ea57af0079038
mirrorclone <86728257+mirrorclone@users.noreply.github.com>
2021-09-13 11:44:11 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -182,25 +182,24 @@ def bot_help(update, context):
 
 
 botcmds = [
-        (f'{BotCommands.HelpCommand}','Get Detailed Help'),
-        (f'{BotCommands.MirrorCommand}', 'Start Mirroring'),
-        (f'{BotCommands.TarMirrorCommand}','Start mirroring and upload as .tar'),
-        (f'{BotCommands.ZipMirrorCommand}','Start mirroring and upload as .zip'),
-        (f'{BotCommands.UnzipMirrorCommand}','Extract files'),
-        (f'{BotCommands.CloneCommand}','Copy file/folder to Drive'),
-        (f'{BotCommands.CountCommand}','Count file/folder of Drive link'),
-        (f'{BotCommands.DeleteCommand}','Delete file from Drive'),
-        (f'{BotCommands.WatchCommand}','Mirror Youtube-dl support link'),
-        (f'{BotCommands.TarWatchCommand}','Mirror Youtube playlist link as .tar'),
-        (f'{BotCommands.CancelMirror}','Cancel a task'),
-        (f'{BotCommands.CancelAllCommand}','Cancel all tasks'),
-        (f'{BotCommands.ListCommand}','Searches files in Drive'),
-        (f'{BotCommands.StatusCommand}','Get Mirror Status message'),
-        (f'{BotCommands.StatsCommand}','Bot Usage Stats'),
-        (f'{BotCommands.PingCommand}','Ping the Bot'),
-        (f'{BotCommands.RestartCommand}','Restart the bot [owner/sudo only]'),
-        (f'{BotCommands.LogCommand}','Get the Bot Log [owner/sudo only]'),
-        (f'{BotCommands.TsHelpCommand}','Get help for Torrent search module')
+        (f'{BotCommands.MirrorCommand}', '🏁 Mirroring'),
+        (f'{BotCommands.TarMirrorCommand}','🔼 Mirroring and upload as .tar'),
+        (f'{BotCommands.ZipMirrorCommand}','🔼 Mirroring and upload as .zip'),
+        (f'{BotCommands.UnzipMirrorCommand}','🗜️ Extract files'),
+        (f'{BotCommands.CloneCommand}','⏫ Copy file/folder to Drive'),
+        (f'{BotCommands.CountCommand}','⏫ Count file/folder of Drive link'),
+        (f'{BotCommands.DeleteCommand}','❌ Delete file from Drive'),
+        (f'{BotCommands.WatchCommand}','📺 Mirror Youtube-dl support link'),
+        (f'{BotCommands.TarWatchCommand}','📺 Mirror Youtube playlist link as .tar'),
+        (f'{BotCommands.CancelMirror}','⏹️ Cancel a task'),
+        (f'{BotCommands.CancelAllCommand}','⏹️ Cancel all tasks'),
+        (f'{BotCommands.ListCommand}','🔍 Searches files in Drive'),
+        (f'{BotCommands.StatusCommand}','🔱 Mirror Status message'),
+        (f'{BotCommands.StatsCommand}','🤖 Bot Usage Stats'),
+        (f'{BotCommands.PingCommand}','🏓 Ping the Bot'),
+        (f'{BotCommands.RestartCommand}','⭕ Restart the bot [owner/sudo only]'),
+        (f'{BotCommands.LogCommand}','🔖 Bot Log [owner/sudo only]'),
+        (f'{BotCommands.TsHelpCommand}','🔎 Torrent search module')
     ]
 
 
